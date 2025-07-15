fb036310151cccaa44d5bde7df454a11a69800aa
Jaime Iglesias <jelmiotambien@gmail.com>
2025-07-15 15:57:31 +0200

    Update ESP32_OpenPLC_HC595_HC165.ino
    Se añaden descripciones

--- ESP32_OpenPLC_HC595_HC165.ino
+++ ESP32_OpenPLC_HC595_HC165.ino
@@ -1,16 +1,29 @@
-#include <ShiftRegister74HC595.h>
-#include <HC165.h>
+//////////////////////////////// sketch section ////////////////////////////////
 
+#include <ShiftRegister74HC595.h>
+#include <HC165.h>  
+
+// create a global shift register object
+// parameters: <number of shift registers> (data pin, clock pin, latch pin)
 ShiftRegister74HC595<1> sr(12, 22, 23);
+
+// Crear un objeto HC165
 HC165 sr165(15, 2, 0);
+
+// Definir los pines HC165
+const int dataPin = 15;    // QH (Salida serial) en el pin 15
+const int clockPin = 2;    // SH_CP (Shift clock) en el pin 2
+const int latchPin = 0;    // ST_CP (Storage clock) en el pin 0
 
 void sketch_setup()
 {
-  sr165.begin();
+  sr165.begin();  // Inicializa el registro de desplazamiento del HC165
 }
+
 
 void sketch_loop()
 {
+  // Salidas de Reles de la placa ES32D26
   int DO0 = 0;
   sr.set(DO0, RELE_1);
   int DO1 = 1;
@@ -28,15 +41,17 @@ void sketch_loop()
   int DO7 = 7;
   sr.set(DO7, RELE_8);
 
-  uint8_t inputVal = sr165.read();
+  // === Leer entradas del HC165 ===
+  uint8_t inputVal = sr165.read();  // Leer los 8 bits de las entradas del HC165
 
-  INT_1 = !((inputVal >> 0) & 1);
-  INT_2 = !((inputVal >> 1) & 1);
-  INT_3 = !((inputVal >> 2) & 1);
-  INT_4 = !((inputVal >> 3) & 1);
-  INT_5 = !((inputVal >> 4) & 1);
-  INT_6 = !((inputVal >> 5) & 1);
-  INT_7 = !((inputVal >> 6) & 1);
-  INT_8 = !((inputVal >> 7) & 1);
+// Asignar los valores leídos a las variables de entrada INT_1 a INT_8, invirtiendo la lógica
+  INT_1 = !((inputVal >> 0) & 1);  // Invertir el bit 0
+  INT_2 = !((inputVal >> 1) & 1);  // Invertir el bit 1
+  INT_3 = !((inputVal >> 2) & 1);  // Invertir el bit 2
+  INT_4 = !((inputVal >> 3) & 1);  // Invertir el bit 3
+  INT_5 = !((inputVal >> 4) & 1);  // Invertir el bit 4
+  INT_6 = !((inputVal >> 5) & 1);  // Invertir el bit 5
+  INT_7 = !((inputVal >> 6) & 1);  // Invertir el bit 6
+  INT_8 = !((inputVal >> 7) & 1);  // Invertir el bit 7
+
 }
-
